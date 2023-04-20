908f6c5e054d89d7f301d08589855deb34e41f7b
ParkerM1 <min17008@byu.edu>
2023-04-20 14:44:59 -0600

    Second prep

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int grade_num = int.Parse(grade);
+
+        if (grade_num >= 90)
+        {
+            Console.WriteLine("Your grade is an A");
+        }
+        else if (grade_num < 90 && grade_num >= 80)
+        {
+            Console.WriteLine("Your grade is an B");
+        }
+        else if (grade_num < 80 && grade_num >= 70)
+        {
+            Console.WriteLine("Your grade is an C");
+        }
+        else if (grade_num < 70 && grade_num >= 60)
+        {
+            Console.WriteLine("Your grade is an D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is an F");
+        }
+        
+        if (grade_num >= 70)
+        {
+            Console.Write("Congrats!! You passed the class!");
+        }
+        else
+        {
+            Console.Write("You did not pass the class :(");
+        }
     }
 }
