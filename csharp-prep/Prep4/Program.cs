4170fda65eeea5d612e629878dbf8f5c30d70940
ParkerM1 <min17008@byu.edu>
2023-04-27 14:35:05 -0600

    finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> guess_nums = new List<int>();
+        int guess_num = 1;
+        while (guess_num != 0)
+        {
+            Console.Write("Please enter a number to add. To end enter 0: ");
+            string guess = Console.ReadLine();
+            guess_num = int.Parse(guess);
+            guess_nums.Add(guess_num);
+        } 
+        int sum = 0;
+        foreach (int num in guess_nums)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum is {sum}");
+
+        int len = (guess_nums.Count);
+        int avg = sum / len;
+        Console.WriteLine($"the average is {avg}");
+
+        int max = guess_nums[0];
+        foreach (int numb in guess_nums)
+        {
+            if (numb > max)
+            {
+                max = numb;
+            }
+        }
+        Console.WriteLine($"The max number is {max}");
     }
 }
