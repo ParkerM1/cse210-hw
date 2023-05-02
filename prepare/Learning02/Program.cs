d1b25c51a2ab431b0d58e0919c5f9b552f3ffbb1
ParkerM1 <min17008@byu.edu>
2023-05-02 16:13:02 -0600

    learning 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        var job1 = new Job();
+        job1.job_title = "Software Enginger";
+        job1.compony = "Mircosoft";
+        job1.start_year = 2011;
+        job1.end_year = 2020;
+
+        var job2 = new Job();
+        job2.job_title = "Software Enginger";
+        job2.compony = "Apple";
+        job2.start_year = 2009;
+        job2.end_year = 2016;        
+
+        Resume my_resume = new Resume();
+        my_resume.name = "allison rose";
+        
+        my_resume.list_job.Add(job1);
+        my_resume.list_job.Add(job2);
+        
+        my_resume.Display();
+    }
+}
+
+class Resume{
+    public string name;
+    public List<Job> list_job = new List<Job>();
+
+    public void Display(){
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine("Jobs:");
+
+        
+        foreach (Job job in list_job)
+        {
+            job.Display();
+        }
+    }
+}
+
+public class Job{
+    public string compony;
+    public string job_title;
+    public int start_year;
+    public int end_year;
+
+    public void Display(){
+        Console.WriteLine($"{job_title} ({compony}) {start_year}-{end_year}");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        var person_one = new Person("layne", "moseley");
+        var person_two = new Person("David", "Hasselhoff");
+        person_one.breathe();
+        person_one.talk();
+        person_two.talk();
+    }
+}
+
+class Person {
+    //attributes
+    string firstname;
+    string lastname;
+
+    // construcotor
+    public Person(string fn, string ln) {
+        firstname = fn;
+        lastname = ln;
+    }
+
+    // behavoirs
+    public void breathe() {
+        Console.WriteLine("breathing");
+    }
+    public void walk() {
+        Console.WriteLine("walking");
+    }
+
+    public void talk() {
+        Console.WriteLine($"hi, my name is {full_name()}");
+    }
+
+    public string full_name(){
+        return firstname + "" + lastname;
     }
 }
