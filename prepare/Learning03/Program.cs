be586d0ba5f12684d1a0a6f4f3ffc0e4442a1756
ParkerM1 <min17008@byu.edu>
2023-05-16 14:51:08 -0600

    fixing prove 2 and prep 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        var savings = new Account();
+        savings.Deposit(500);
+        savings.Deposit(1000);
+    }
+}
+
+
+public class Account {
+    private List<int> transactoins = new List<int>();
+    
+    public void Deposit (int amount){
+        transactoins.Add(amount);
+    }
+
+    public int Balance(){
+        var balance = 0;
+        foreach (var transactoin in transactoins){
+            balance += transactoin;
+        }
+
+        return balance;
     }
 }
